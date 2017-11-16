4ab135dc0bbcba3f6bd00ab051e8b9faa52fcaef
agoalofalife <agoalofalife@gmail.com>
2017-11-16 09:52:18 +0300

    pre load

--- src/app/components/pages/collection/index.js
+++ src/app/components/pages/collection/index.js
@@ -1,6 +1,7 @@
 let Node = require('basis.ui').Node;
 let dataCollection = require('app.type.collection')
-
+let STATE = require('basis.data').STATE;
+let Value = require('basis.data').Value;
 
 module.exports = new Node({
     // active: basis.PROXY,
@@ -14,8 +15,16 @@ module.exports = new Node({
          data : {
             title : "test"
         },
+          action : {
+            chooseCollection(target) {
+                console.log('Hello it me ', this.data.title)
+            }
+        }
     },
-    
+    binding : {
+        loading: Value.query('childNodesState').as(state => state == STATE.PROCESSING),
+    },
+
     // handler: {
     //     activeChanged() {
     //         if (this.active) {
