dfa8dafa5c4cd39d96c162422a743568f2af8c14
agoalofalife <agoalofalife@gmail.com>
2017-11-20 00:03:29 +0700

    add pre loading

--- src/app/components/menu/index.js
+++ src/app/components/menu/index.js
@@ -2,9 +2,9 @@ let Node = require('basis.ui').Node;
 let router = require('basis.router');
 let pages = require('../pages/index');
 let dataCollection = require('app.type.collection')
-let dataDocument= require('app.type.document')
-let documents = require('app.components.menu.document.index')
-var ajax = basis.require('basis.net.ajax');
+let ajax = basis.require('basis.net.ajax');
+let Value = require('basis.data').Value;
+let STATE = require('basis.data').STATE;
 
 module.exports = new Node({
     template: resource('./template.tmpl'),
@@ -22,22 +22,13 @@ module.exports = new Node({
                     },
                     handler: {
                         success: function(transport, request, response){
-                            var obj = {
-                                name:'John Doe',
-                                age: 20,
-                                children:[{name:'Jack', age:5}, {name:'Ann', age:8}],
-                                wife:{name:'Jane Doe', age:28 }
-                            }
-
-                            var node = new PrettyJSON.view.Node({
-                                el:$('.json_area'),
+                            new PrettyJSON.view.Node({
+                                el:document.querySelector('.json_area'),
                                 data:response
                             });
                         },
                     }
                 });
-
-
             }
         },
     },
@@ -47,11 +38,8 @@ module.exports = new Node({
     },
     binding: {
         json : 'data:',
-        // documents: 'satellite:',
+        loading:Value.query('childNodesState').as(state => state == STATE.PROCESSING),
         contentCollection:'data:',
-    },
-    satellite: {
-        documents: documents
     },
     dataSource: dataCollection,
 });
