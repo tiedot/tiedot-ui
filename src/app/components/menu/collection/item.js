e0fd1ca05b8334ed8294eb60011937c55ec3fa31
agoalofalife <agoalofalife@gmail.com>
2017-11-20 20:05:47 +0700

    add selected in menu collection

--- src/app/components/menu/collection/item.js
+++ src/app/components/menu/collection/item.js
@@ -1,22 +1,23 @@
 let Node = require('basis.ui').Node;
 let ajax = basis.require('basis.net.ajax');
+let Value = require('basis.data').Value;
+let currentCollection = new Value();
 
 module.exports = Node.subclass({
     template: resource('./item.tmpl'),
-
+    selected: currentCollection.compute((child, name) => {
+        return child.data.title === name;
+    }),
     binding: {
         title: 'data:',
     },
     action: {
         click() {
-            console.log( this );
+            currentCollection.set(this.data.title);
             ajax.request({
                 url: 'http://localhost:8080/getpage?col=' + this.data.title  + '&page=0&total=1',
-                params: {
-                    userId: 123
-                },
                 handler: {
-                    success: function(transport, request, response){
+                    success: (transport, request, response) => {
                         new PrettyJSON.view.Node({
                             el:document.querySelector('.json_area'),
                             data:response
